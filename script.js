64d36d7c42c50c6f19f45ca3c5e14a06224bc855
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-02-21 11:05:23 +0000

    finalised version of JS to display the test on the page

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
 const textsToCopyContainer = document.querySelector('#textsToCopyContainer')
-
-function createParagraphs(name) {
-    let div = document.createElement('div');
-    div.innerHTML = name;
-    return div;
-}
+const paragraphs = []
 
 fetch('https://flipsum-ipsum.net/api/icw/v1/generate?ipsum=recipe-ipsum-text-generator&start_with_fixed=0&paragraphs=4').then((response) => {
     return response.json()
 }).then((data) => {
-    console.log(data) 
-    let paragraphOne = data[0].split(' ')
-    let paragraphTwo = data[1].split(' ')
-    let paragraphThree = data[2].split(' ')
-    let paragraphFour = data[3].split(' ')
-    const paragraphs = [paragraphOne, paragraphTwo, paragraphThree, paragraphFour]
-
-    console.log(paragraphOne)
-    let referenceTextArray = paragraphOne.concat(paragraphTwo, paragraphThree, paragraphFour)
-
-
-    console.log(referenceTextArray)
-
-    console.log(referenceTextArray[0])
-
-    let count = 0
-    for(let indexParagraph = 0; indexParagraph< 4; indexParagraph++) {
-        let stringToPrint = '<p id="' + indexParagraph + '">'
+    data.forEach((textOutput) => {
+        paragraphs.push(textOutput.split(' '))
+    }) 
     
-        for(let indexWord = 0; indexWord < paragraphs[indexParagraph].length; indexWord++) {
-            stringToPrint = stringToPrint + '<span id="' + count + '"> ' + paragraphs[indexParagraph][indexWord] + ' </span>'
+    const referenceTextArray = paragraphs[0].concat(paragraphs[1], paragraphs[2], paragraphs[3]) // for use in future stories
+    let count = 0
+    paragraphs.forEach((paragraph, indexParagraph) => {
+        let stringToPrint = '<p id="paragraph' + indexParagraph + '">'
+        
+        paragraph.forEach((word) => {
+            stringToPrint = stringToPrint + '<span id="' + count + '"> ' + word + ' </span>'
             count++
-        }
+        })
         stringToPrint += '</p>'
-        textsToCopyContainer.innerHTML += stringToPrint   
-    }
+        textsToCopyContainer.innerHTML += stringToPrint
+    })
 })
+
+
+
+
