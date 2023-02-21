7b21eaf22a2a984dd2d968462accaec72f563e22
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-02-21 10:03:50 +0000

    we managed to display all the paragraphs with the words in different spans

--- script.js
+++ script.js
@@ -14,48 +14,25 @@ fetch('https://flipsum-ipsum.net/api/icw/v1/generate?ipsum=recipe-ipsum-text-gen
     let paragraphTwo = data[1].split(' ')
     let paragraphThree = data[2].split(' ')
     let paragraphFour = data[3].split(' ')
+    const paragraphs = [paragraphOne, paragraphTwo, paragraphThree, paragraphFour]
 
     console.log(paragraphOne)
     let referenceTextArray = paragraphOne.concat(paragraphTwo, paragraphThree, paragraphFour)
+
 
     console.log(referenceTextArray)
 
     console.log(referenceTextArray[0])
 
-    for(i = 0; i < 4; i++) {
-        //createlement
-        let count = 0
-        for(j = 0;j <= paragraphOne.length; j++) {
-            
+    let count = 0
+    for(let indexParagraph = 0; indexParagraph< 4; indexParagraph++) {
+        let stringToPrint = '<p id="' + indexParagraph + '">'
+    
+        for(let indexWord = 0; indexWord < paragraphs[indexParagraph].length; indexWord++) {
+            stringToPrint = stringToPrint + '<span id="' + count + '"> ' + paragraphs[indexParagraph][indexWord] + ' </span>'
             count++
         }
-    }
-
-
-    for(const[indexParagraph, paragraph] of data.entries()) {
-        let stringToPrint = '<p id="' + indexParagraph + '">'
-        const words = paragraph.split() 
-        for(const[indexWord, word] of words.entries()) {
-            stringToPrint = stringToPrint + '<span id="' + indexWord + '">' + word + '</span>'
-            
-        } 
         stringToPrint += '</p>'
-        textsToCopyContainer.innerHTML += stringToPrint
+        textsToCopyContainer.innerHTML += stringToPrint   
     }
-    // data.forEach((paragraph) => {
-    //     let splitParagraph = paragraph.split(' ')
-    //     splitParagraph.forEach((word) => {
-
-    //     })
-    })
-
-
-//split each string
-//data[0].split
-
-//i=0; i<4; i++ {
-
- // innerHTML  "<p id" + i
-//
-//(j =0 )
- // <span j+i; 
+})
