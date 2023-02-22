1138a95dd9c232cf911b94fb8e27d1746054c010
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-02-22 10:34:42 +0000

    pseudo-coding this task

---
+++ s3_t1.js
@@ -0,0 +1,48 @@
+const textsToCopyContainer = document.querySelector('#textsToTypeContainer')
+const paragraphs = []
+
+fetch('https://flipsum-ipsum.net/api/icw/v1/generate?ipsum=recipe-ipsum-text-generator&start_with_fixed=0&paragraphs=4').then((response) => {
+    return response.json()
+}).then((data) => {
+    data.forEach((textOutput) => {
+        //the line below replaces any accented character by the matching non-accented character
+        //e.g. "é" becomes "e"
+        textOutput = textOutput.normalize("NFD").replace(/\p{Diacritic}/gu, "")
+        paragraphWordsArray =  textOutput.split(' ')
+        paragraphs.push(paragraphWordsArray)
+    }) 
+    
+    const referenceTextArray = paragraphs[0].concat(paragraphs[1], paragraphs[2], paragraphs[3]) // for use in future stories
+    let count = 0
+    paragraphs.forEach((paragraph, indexParagraph) => {
+        let stringForHtml = '<p class="textParagraph" id="paragraph' + indexParagraph + '">'
+        
+        paragraph.forEach((word) => {
+            stringForHtml = stringForHtml + '<span id="' + count + '"> ' + word + ' </span>'
+            count++
+        })
+        stringForHtml += '</p>'
+        textsToCopyContainer.innerHTML += stringForHtml
+    })
+})
+
+let count = -1
+let numberValidWords = 0
+
+document.addEventListener('keyup', event => {
+    if (event.code === 'Space') {
+      console.log('Space pressed')
+    }
+})
+
+// event listener for space bar { 
+//      count++
+//      wordFromPlayer = document.query("input")
+//      if(wordFromPlayer.value === referenceArray[count]) 
+//      if(wordIsValid){
+ //         numberValidWords++} // for later                 
+//      wordFromPlayer.value = ''
+        
+        // wordToChange = document.querySelector("#"+count)
+        // wordToChange.ClassList.add("valid")    
+//}
