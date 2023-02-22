62c48595f28554becd11f89dd811a380853d1e6b
Robin Hodge <mrrobinhodge@gmail.com>
2023-02-22 16:37:40 +0000

    First run working fully!

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const textToTypeContainer = document.querySelector('#textToTypeContainer')
+let textInput = document.querySelector('#textInput')
 let referenceTextArray = []
+let wordIsValid = false
+let wordIndexCount = -1
+let numberOfValidWords = 0
+let scrollPixels = 0
+let totalLengthOfWords = 0
+let upcomingWordLength = 0
 
 fetch('https://flipsum-ipsum.net/api/icw/v1/generate?ipsum=recipe-ipsum-text-generator&start_with_fixed=0&paragraphs=4').then((response) => {
     return response.json()
@@ -8,10 +15,10 @@ fetch('https://flipsum-ipsum.net/api/icw/v1/generate?ipsum=recipe-ipsum-text-gen
         //the line below replaces any accented character by the matching non-accented character
         //e.g. "é" becomes "e"
         textOutput = textOutput.normalize("NFD").replace(/\p{Diacritic}/gu, "")
-        wordsArray =  textOutput.split(' ')
+        wordsArray = textOutput.split(' ')
         referenceTextArray = referenceTextArray.concat(wordsArray)
-    }) 
-    
+    })
+
     let count = 0
     let stringForHtml = '<p class="textParagraph">'
     referenceTextArray.forEach((word) => {
@@ -20,31 +27,50 @@ fetch('https://flipsum-ipsum.net/api/icw/v1/generate?ipsum=recipe-ipsum-text-gen
     })
     stringForHtml += '</p>'
     textToTypeContainer.innerHTML += stringForHtml
+    let upcomingWord = document.getElementById(0)
+    upcomingWord.style.color = ("orange")
+    upcomingWordLength = document.getElementById(0).clientWidth
+    totalLengthOfWords += upcomingWordLength
 })
 
-let wordIsValid = false
-let wordIndexCount = -1
-let numberOfValidWords = 0
 
-let textInput = document.querySelector('#textInput')
 
 textInput.addEventListener('keyup', event => {
-    if(event.code === 'Space') {
+    if (event.code === 'Space') {
         let textValue = textInput.value
         let textValueWithoutSpace = textValue.slice(0, textValue.length - 1);
         wordIndexCount++
         wordIsValid = (textValueWithoutSpace === referenceTextArray[wordIndexCount])
 
-        if(wordIsValid) {
+        let wordJustFinished = document.getElementById(wordIndexCount)
+        if (wordIsValid) {
             numberOfValidWords++ // for later use
             textInput.value = ''
-        } else{
+            wordJustFinished.style.color = ("green")
+        } else {
             textInput.value = ''
+            wordJustFinished.style.color = ("red")
         }
+
+        let upcomingWord = document.getElementById(wordIndexCount + 1)
+        
+        upcomingWordLength = document.getElementById(wordIndexCount + 1).clientWidth //again, not sure how well named this is
+        totalLengthOfWords += upcomingWordLength
+
+        if (totalLengthOfWords >= 864.1) {
+            
+            totalLengthOfWords = 0
+            totalLengthOfWords += upcomingWordLength
+            document.getElementById("textToTypeContainer").scroll({
+                top: scrollPixels += 90.75,
+                behavior: 'smooth'
+            })
+        
+        }
+        upcomingWord.style.color = ("orange")
+
     }
 })
-
-
 
 
 
